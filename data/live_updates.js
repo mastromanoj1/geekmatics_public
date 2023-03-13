4f41137c02d694f94303c159772ba33529d68609
mastromanoj1 <mastromanoj1@gmail.com>
2023-03-13 15:39:50 +0530

    live

--- data/live_updates.js
+++ data/live_updates.js
@@ -1,6 +1,6 @@
 module.exports = [
     "😊 Happy to welcome you all to Geekmatics-23, \n\n📌 Make sure to express your talents and cherish the day with other Geeks.\n\n <b>இப்படிக்கு</b> \n போஸ்பாண்டி ",
     "⌛ The Doors for the event Code_Racer will open by 11:00AM , The registered geeks are asked to assemble in the room number <b> 196 </b>. ",
-    "⚔️ <b><i>Hey Geeks</i></b>, \n We suggest you to use your food coupons now as we are expecting crowd in food stalls after 1:00pm.   "
+    // "⚔️ <b><i>Hey Geeks</i></b>, \n We suggest you to use your food coupons now as we are expecting crowd in food stalls after 1:00pm.   "
     
 ]
